75e0ad3f0ad8fdab16a8dd25c16ab5491d388d68
Sania-Akhtar-SA <saniaakhtar1712@gmail.com>
2024-02-09 02:06:22 +0530

    Polymorphism

---
+++ Polymorphism.java
@@ -0,0 +1,27 @@
+class Fruits{
+    public void Taste(){
+        System.out.println("Fruits are Tasty.");
+    }
+}
+class Mango extends Fruits{
+    public void Taste(){
+        System.out.println("Mango are sweet in taste. ");
+    }
+}
+class Strawberry extends Fruits{
+    public void Taste(){
+        System.out.println("Strawberry are sweet and sour in taste. ");
+    }
+}
+
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Fruits f = new Fruits();
+        Mango m = new Mango();
+        Strawberry s = new Strawberry();
+        f.Taste();
+        m.Taste();
+        s.Taste();
+    }
+}
